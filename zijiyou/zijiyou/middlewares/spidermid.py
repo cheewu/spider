8028aaf73267797ee0e5d1c80a3a6184ba9a68be
hy <hy@ubuntu.ubuntu-domain>
2011-05-13 14:08:07 +0800

    将urlDump改成set类型

--- zijiyou/zijiyou/middlewares/spidermid.py
+++ zijiyou/zijiyou/middlewares/spidermid.py
@@ -25,8 +25,7 @@ class DuplicateUrlFilter(object):
     def __init__(self):
         '''init the dump of url which request successful'''
         self.mon=MongoDbApt()
-#        self.urlDump=set()
-        self.urlDump=[]
+        self.urlDump=set()
         self.colName=settings.get('CRAWL_DB')
         if not self.colName:
             log.msg('没有配置CRAWL_DB！，请检查settings', level=log.ERROR)
@@ -39,14 +38,14 @@ class DuplicateUrlFilter(object):
         responses=self.mon.findFieldsAndSort('ResponseBody', whereJson={}, fieldsJson={'pageUrl':1})
         log.msg('完成ResponseBody加载.从CrawlUrl加载%s个；从ResponseBody加载%s个；时间：%s' %(len(crawlUrls),len(responses),datetime.datetime.now()), level=log.INFO)
         for p in crawlUrls:
-            if "url" in p and (not p['url'] in self.urlDump):
-                self.urlDump.append(p['url'])
+#            if "url" in p and (not p['url'] in self.urlDump):
+            self.urlDump.add(p['url'])
 #                fp=self.getFingerPrint(p['url'])
 #                if not fp in self.urlDump:
 #                    self.urlDump.add(fp)
         for p in responses:
-            if "pageUrl" in p and (not p['pageUrl'] in self.urlDump):
-                self.urlDump.append(p['pageUrl'])
+#            if "pageUrl" in p and (not p['pageUrl'] in self.urlDump):
+            self.urlDump.add(p['pageUrl'])
 #                fp=self.getFingerPrint(p['pageUrl'])
 #                if not fp in self.urlDump:
 #                    self.urlDump.add(fp)
@@ -54,6 +53,7 @@ class DuplicateUrlFilter(object):
     
     def process_spider_output(self, response, result, spider):
         '''drop the request which appear in urlDump'''
+        log.msg("开始排重", level=log.INFO)
         newResult=[]
         counter=0
         for p in result:
@@ -61,12 +61,14 @@ class DuplicateUrlFilter(object):
             if isinstance(p, Request):
                 if p.url and (p.url in self.urlDump):
 #                    fp=self.getFingerPrint(p.url)
-                    if p.url in self.urlDump:
+                    preCount = len(self.urlDump)
+                    self.urlDump.add(p.url)
+                    if preCount == len(self.urlDump):
                         log.msg("排除重复 url=%s" % (p.url), level=log.DEBUG)
                         continue
                     else:
                         #更新urlDump
-                        self.urlDump.append(p.url)
+#                        self.urlDump.append(p.url)
 #                        self.urlDump.add(fp)
                         
                         #保存到数据库
@@ -100,9 +102,11 @@ class DuplicateUrlFilter(object):
         responseStatus=response.status
         if responseStatus  in range(199,305) :
             dupUrl=response.url
-            if not dupUrl in self.urlDump:
+            preCount = len(self.urlDump)
+            self.urlDump.add(dupUrl)
+            if preCount < len(self.urlDump):
+#            if not dupUrl in self.urlDump:
                 log.msg("new url=%s" % dupUrl, level=log.INFO)
-                self.urlDump.append(dupUrl)
         
 class SaveNewRequestUrl(object):
     '''
