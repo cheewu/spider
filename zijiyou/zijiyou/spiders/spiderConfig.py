08203bf39d4b37180ed5c6c475ebe172e973b05c
hy <hy@ubuntu.ubuntu-domain>
2011-05-17 11:41:36 +0800

    修改游记下一页的正则表达式

--- zijiyou/zijiyou/spiders/spiderConfig.py
+++ zijiyou/zijiyou/spiders/spiderConfig.py
@@ -69,7 +69,7 @@ spiderConfig = {
                                     
 #                                    {'regex':r'(http://www.lvping.com/)?(journals-)+d\d+-s\d+-p\d+-g/\w+\.html$', 'priority':1}, #攻略列表
                                     {'regex':r'(http://www.lvping.com)?(/members/)+(\w/)+journals$', 'priority':700},# 会员游记列表
-                                    {'regex':r'(http://www.lvping.com)?/Journals.aspx\?type=1.*selecttype=0.*', 'priority':700},# 精品游记列表
+                                    {'regex':r'(http://www.lvping.com)?/Journals.aspx\?.*selecttype=0.*', 'priority':700},# 游记列表
                                     {'regex':r'(http://www.lvping.com)?/Journals.aspx\?.*selecttype=2.*', 'priority':700},# 攻略列表
                                     {'regex':r'(http://www.lvping.com/)?(travel-)+d\d+-\w+\.html$', 'priority':400},    #常识列表页1
                                     {'regex':r'(http://www.lvping.com/)?(travel-)+d\d+-\w+:brochure\.html#\w+', 'priority':400} #常识列表页2
