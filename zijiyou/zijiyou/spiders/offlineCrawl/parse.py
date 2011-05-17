8adf1da02f7c632abe2ad0420e42d584a94fc0a5
hy <hy@ubuntu.ubuntu-domain>
2011-05-17 17:21:47 +0800

    添加parse结束日志

--- zijiyou/zijiyou/spiders/offlineCrawl/parse.py
+++ zijiyou/zijiyou/spiders/offlineCrawl/parse.py
@@ -111,6 +111,8 @@ class Parse(object):
             self.parse()
         
         #关闭日志
+        self.parseLog('parse 完成', level=LogLevel.INFO)
+        self.loger.close()
         if self.loger.closed :
             self.loger.close()
             print 'OK'
