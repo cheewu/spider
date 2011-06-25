cfaeb39e473972b839ff9ff8e5790eb6928e4429
shiym <shiym@shiym-virtual-machine>
2011-06-25 21:13:32 +0800

    BBS爬虫
    Change-Id: I000000000000000000000032436wer6700000

---
+++ zijiyou/zijiyou/spiders/go2euSpider.py
@@ -0,0 +1,14 @@
+'''
+Created on 2011-6-25
+
+@author: shiym
+'''
+from zijiyou.spiders.baseCrawlSpider import BaseCrawlSpider
+
+class Go2euSpider(BaseCrawlSpider):
+    '''
+    www.go2euSpider.com
+    '''
+    name ="go2euSpider"
+    
+SPIDER = Go2euSpider()
