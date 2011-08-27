bb385cb14244eaeeb789ad97bf1d508538280c4c
shiym <shiym@shiym-virtual-machine>
2011-08-27 13:24:09 +0800

    去除了debug信息

--- zijiyou/zijiyou/spiders/baseSeSpider.py
+++ zijiyou/zijiyou/spiders/baseSeSpider.py
@@ -188,25 +188,27 @@ class BaseSeSpider(BaseCrawlSpider):
         if blocks==None or len(blocks)<1:
             log.msg("没有抓取到任何目标页豆腐块！resultItemLinkXpath：%s；url：%s" % (resultItemLinkXpath,response.url), level=log.ERROR)
             return reqs
-#        #第一页搜索结果抽取出总搜索结果数，生成剩余list页的request
-#        if self.urlPatternMeta in response.meta:
-#            pageRequests = self.makeListRequestByFirstPageForSEs(response, len(blocks))
-#            if pageRequests and len(pageRequests):
-#                reqs.extend(pageRequests)
-#                log.msg("第一页：%s，生成剩余的搜索页面数为：：%s" % (response.url, len(pageRequests)), level=log.INFO)
-#            else:
-#                log.msg("第一页没有生成其他list搜素页，可能是搜素结果不足一页。url：%s" % response.url, level=log.INFO)
+        #第一页搜索结果抽取出总搜索结果数，生成剩余list页的request
+        if self.urlPatternMeta in response.meta:
+            pageRequests = self.makeListRequestByFirstPageForSEs(response, len(blocks))
+            if pageRequests and len(pageRequests):
+                reqs.extend(pageRequests)
+                log.msg("第一页：%s，生成剩余的搜索页面数为：：%s" % (response.url, len(pageRequests)), level=log.INFO)
+            else:
+                log.msg("第一页没有生成其他list搜素页，可能是搜素结果不足一页。url：%s" % response.url, level=log.INFO)
         #抽取item页的link，同时传递发表时间、摘要等信息
         xpathItems = self.config['seXpath'][response.meta['seName']]
         itemLinks=[]
         if 'urlRegex' in  xpathItems:
             body=response.body_as_unicode().encode('utf-8')
-            print '转吗长度%s' % len(body)
-            matchs=re.findall(r'<!-- <a href="(\S*)" target="_blank" class=m>正文快照</a>', body)
+            matchs=re.findall(xpathItems['urlRegex'], body)
             for p in matchs:
                 print p
                 itemLinks.append(p)
-            log.msg('直接从搜素结果list页里得到快照的链接数：%s,regex:%s ,url:%s' % (len(itemLinks),xpathItems['urlRegex'],response.url), level=log.INFO)
+            if len(itemLinks)<1:
+                log.msg('没有直接从搜素结果list页里得到快照的链接,regex:%s ,url:%s' % (xpathItems['urlRegex'],response.url), level=log.ERROR)
+            else:
+                log.msg('直接从搜素结果list页里得到快照的链接数：%s,regex:%s ,url:%s' % (len(itemLinks),xpathItems['urlRegex'],response.url), level=log.INFO)
             
         for index in range(0,len(blocks)):
             block=blocks[index]
