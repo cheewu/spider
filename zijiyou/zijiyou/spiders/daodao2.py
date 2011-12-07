828d35e9f4d5abeeba36250ffa5af00d4cbed736
shiym <shiym@shiym-virtual-machine>
2011-12-07 21:26:25 +0800

    新的daodao爬虫

---
+++ zijiyou/zijiyou/spiders/daodao2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2011-3-28
+
+@author: shiym
+'''
+from zijiyou.spiders.baseCrawlSpider import BaseCrawlSpider
+class Daodao2(BaseCrawlSpider):
+    '''
+    Spider for www.daodao.com
+    '''
+    name ="daodaoSpider2"
+    
+SPIDER = Daodao2()
