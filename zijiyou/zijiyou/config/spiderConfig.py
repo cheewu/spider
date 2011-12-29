187654b7cd5d098460b115a62bae4ea12c4a63b7
shiym <shiym@ubuntu>
2011-12-28 23:51:34 -0800

    sozhen configuration

--- zijiyou/zijiyou/config/spiderConfig.py
+++ zijiyou/zijiyou/config/spiderConfig.py
@@ -643,85 +643,101 @@ spiderConfig = {
     "sozhenSpider":{
         'allowedDomains':["sozhen.com"],
         'startUrls':[
-            'http://www.sozhen.com/default/chinaprovince.html', #中国古镇
-        'http://www.sozhen.com/default/newsarticle.html' #古镇新闻
-        ],
+                     'http://www.sozhen.com/default/newsarticle.html', #古镇新闻
+	                 'http://www.sozhen.com/default/townarticle_2.html',
+	                 'http://www.sozhen.com/default/townarticle_3.html',
+	                 'http://www.sozhen.com/default/townarticle_6.html',
+	                 'http://www.sozhen.com/default/townarticle_24.html',
+	                 'http://www.sozhen.com/default/townarticle_43.html',
+	                 'http://www.sozhen.com/default/townarticle_45.html',
+	                 'http://www.sozhen.com/default/townarticle_46.html',
+	                 'http://www.sozhen.com/default/townarticle_47.html',
+	                 'http://www.sozhen.com/default/townarticle_48.html',
+	                 'http://www.sozhen.com/default/townarticle_51.html',
+	                 'http://www.sozhen.com/default/townarticle_52.html',
+	                 'http://www.sozhen.com/default/townarticle_53.html',
+	                 'http://www.sozhen.com/default/townarticle_54.html',
+	                 'http://www.sozhen.com/default/townarticle_55.html',
+	                 'http://www.sozhen.com/default/townarticle_56.html',
+	                 'http://www.sozhen.com/default/townarticle_57.html',
+	                 'http://www.sozhen.com/default/townarticle_58.html',
+	                 'http://www.sozhen.com/default/townarticle_59.html',
+	                 'http://www.sozhen.com/default/townarticle_60.html',
+	                 'http://www.sozhen.com/default/townarticle_61.html',
+	                 'http://www.sozhen.com/default/townarticle_88.html',
+	                 'http://www.sozhen.com/default/townarticle_89.html',
+	                 'http://www.sozhen.com/default/townarticle_98.html',
+	                 'http://www.sozhen.com/default/townarticle_155.html',
+                     ],
         #普通list页正则表达式
         'normalRegex':[
-            {
-        #某个古镇列表，如：http://www.sozhen.com/default/chinatown_12.html
-                'regex':r'^chinatown_\d+\.html$',
-                'priority':1000
-            },
-        {
-        #古镇新闻列表：分页，如：http://www.sozhen.com/default/newsarticle_0_10.html
-                'regex':r'^newsarticle_0_\d+\.html$',
-                'priority':1000
-            },
-        {
-        #某个客栈游记攻略列表（含分页），如：
-            #http://www.sozhen.com/default/towninn_12.html
-            #http://www.sozhen.com/default/towninn_12_7.html
-                'regex':r'^/default/towninn_\d+(_\d+)?\.html$',
-                'priority':1000
-            },
-        {
-        #某个古镇游记攻略列表（含分页），如：
-            #http://www.sozhen.com/default/townarticle_62.html
-            #http://www.sozhen.com/default/townarticle_62_1.html
-                'regex':r'^\.\./default/townarticle_\d+(_\d+)?\.html$',
-                'priority':1000
-            },            
-        ],
+                       #古镇攻略下一页
+                       {
+                        'regex':r'townarticle.*html',
+                        'region':r'//div[2]/div[2]/div[2]/div[2]/div[37]/div',
+                        'priority':1000
+    	               },
+                       {
+                        #古镇新闻列表：分页，如：http://www.sozhen.com/default/newsarticle_0_10.html
+                        'regex':r'^newsarticle_0_\d+\.html$',
+                        'priority':1000
+                        },
+                       {
+                        #某个客栈游记攻略列表（含分页），如：
+                        'regex':r'^/default/towninn_\d+(_\d+)?\.html$',
+                        'priority':1000
+                        },
+                       ],
         #item页正则表达式 itemCollectionName对应item存放的数据表名
         'itemRegex':[
-            #Article
-            {
-        #文章内容，如：http://www.sozhen.com/default/townarticlecon_12_52358.html
-        'itemCollectionName':'Article',
-        'regex':r'^/default/\w+con_\d+_\d+\.html$',
-        'priority':600
-            },  
-        ]
-    },
+                     #Article
+                     {
+                      #文章内容，如：http://www.sozhen.com/default/townarticlecon_12_52358.html
+                      'itemCollectionName':'Article',
+	                  'region':'//div[2]/div[2]/div[2]/div[2]',
+                      'regex':r'/default/\w+con_\d+_\d+\.html$',
+                      'priority':600
+                      },  
+                     ]
+                    },
 
 
     "21cnSpider":{
         'allowedDomains':["travel.21cn.com"],
         'startUrls':[
-            'http://travel.21cn.com/'
-        ],
+                     'http://travel.21cn.com/'
+                    ],
         #普通list页正则表达式
         'normalRegex':[
-            {
-        #列表，如：http://travel.21cn.com/scene/foreign/list1.shtml
-                'regex':'^http://travel\.21cn\.com/.*/list\d+\.shtml$',
-                'priority':1000
-            },
-        {
-        #列表（含分页），如：http://travel.21cn.com/scene/foreign/list2.shtml
-                'regex':'^list\d+\.shtml$',
-                'priority':1000
-            }
-        ],
+                       {
+                        #列表，如：http://travel.21cn.com/scene/foreign/list1.shtml
+                        'regex':'^http://travel\.21cn\.com/.*/list\d+\.shtml$',
+                        'priority':1000
+                        },
+                       {
+                        #列表（含分页），如：http://travel.21cn.com/scene/foreign/list2.shtml
+                        'regex':'^list\d+\.shtml$',
+                        'priority':1000
+                        }
+                       ],
         #item页正则表达式 itemCollectionName对应item存放的数据表名
         'itemRegex':[
-            #Article
-            {
-        #文章页面，如：http://travel.21cn.com/outdoor/tubu/2007/08/17/3422364.shtml
-                'itemCollectionName':'Article',
-                'regex':'http://travel\.21cn\.com/\w+/\w+/\d+/\d+/\d+/\d+\.shtml',
-                'priority':600
-            },
-            #Article  
-            {
-        #含分页的文章，如：http://travel.21cn.com/guide/senery/2011/09/27/9268652_2.shtml
-                'itemCollectionName':'Article',
-                'regex':'^\d+_\d+\.shtml$',
-                'priority':600
-            }  
-        ]
-    },
+                     #Article
+                     {
+                      #文章页面，如：http://travel.21cn.com/outdoor/tubu/2007/08/17/3422364.shtml
+                      'itemCollectionName':'Article',
+                      'regex':'http://travel\.21cn\.com/\w+/\w+/\d+/\d+/\d+/\d+\.shtml',
+                      'priority':600
+                      },
+                     #Article  
+                     {
+                      #含分页的文章，如：http://travel.21cn.com/guide/senery/2011/09/27/9268652_2.shtml
+                      'itemCollectionName':'Article',
+                      'regex':'^\d+_\d+\.shtml$',
+                      'priority':600
+                      }  
+                     ]
+                  },
 
     "meishiSpider":{
         'allowedDomains':["meishiditu.com" ],
