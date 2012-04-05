6f6bd73570ca5129ffe41821943ee20457bcdff0
ubuntu <ubuntu@dev.zijiyou.com>
2012-04-05 11:11:59 +0800

    add spidermain.py parsemain.py

---
+++ zijiyou/spidermain.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#from scrapy.cmdline import execute
+#from zijiyou.common.extractText import Extracter
+from scrapy.cmdline import execute
+from zijiyou.common.extractText import Extracter
+from zijiyou.spiders.offlineCrawl.parse import Parse
+import sys
+#import re
+
+#解析
+def beginparse(argv=None):
+    if argv == None:
+        argv = sys.argv
+        argv = argv[1:]
+        print argv
+    if len(argv) > 0:
+        p = Parse()
+        p.parse(spiderName = argv)
+#爬虫
+def beginspider(argv=None): 
+    if argv == None:
+        argv = sys.argv
+        argv = argv[1:]
+        print argv
+    if len(argv) >= 1:
+        execute(argv=['scrapy','crawl',argv[0]])
+    else:
+        print '必须提供至少一个爬虫的名字'
+    
+if __name__ == '__main__':
+    beginspider()
+    
